2091e43f2e33ac66fb744a2b37bb129a3c515f44
Koji Oishi <39555285+jiko797torayo@users.noreply.github.com>
2021-07-10 00:25:20 +0900

    pageの作成

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/molecules/SearchInput";
-import { UserCard } from "./organisms/user/UserCard";
+import { Router } from "./router/Router";
 import "./styles.css";
-import { HeaderOnly } from "./templates/HeaderOnly";
-import { DefaultLayout } from "./templates/DefaultLayout";
 
-const user = {
-  name: "じゃけぇ",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "a@a.com",
-  phone: "090-1234-5678",
-  company: {
-    name: "株式会社にほん"
-  },
-  website: "https://google.com"
-};
 export default function App() {
-  return (
-    <BrowserRouter>
-      <DefaultLayout className="App">
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>テスト</SecondaryButton>
-        <br />
-        <SearchInput />
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
---
+++ src/components/pages/Users.jsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { UserCard } from "../../organisms/user/UserCard";
+import { SearchInput } from "../molecules/SearchInput";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    id: val,
+    name: `じゃけ${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    email: "a@a.com",
+    phone: "090-1234-5678",
+    company: {
+      name: "株式会社にほん"
+    },
+    website: "https://google.com"
+  };
+});
+
+export const Users = () => {
+  return (
+    <SContainer>
+      <h2>ユーザー一覧ページです</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={user.id} user={user} />
+        ))}
+      </SUserArea>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
+`;
