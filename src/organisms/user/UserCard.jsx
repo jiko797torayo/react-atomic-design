3df988f202db6a6b15c73dbf38740cbbccda62f3
Koji Oishi <39555285+jiko797torayo@users.noreply.github.com>
2021-07-09 19:48:30 +0900

    organismの作成1

---
+++ src/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>a@a.com</dd>
+        <dt>TEL</dt>
+        <dd>090-1234-5678</dd>
+        <dt>会社名</dt>
+        <dd>株式会社にほん</dd>
+        <dt>WEB</dt>
+        <dd>https://nihon-inc.com</dd>
+      </dl>
+    </div>
+  );
+};
